17c4ae3c50ebb86912c67d78f0f8bb469dbd0e2d
רננה כהן <rencohen@g.jct.ac.il>
2021-04-25 20:58:03 +0300

    nng

--- test/geometries/GeometriesTest.java
+++ test/geometries/GeometriesTest.java
@@ -1,11 +1,13 @@
 package geometries;
 
+
 import org.junit.jupiter.api.Test;
 import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class GeometriesTest {
 
