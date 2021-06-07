a31fd4c61994b1dc8dcebab459636fdf8b1e3726
רננה כהן <rencohen@g.jct.ac.il>
2021-06-07 20:34:03 +0300

    eize yofi1

--- test/renderer/ReflectionRefractionTests.java
+++ test/renderer/ReflectionRefractionTests.java
@@ -190,6 +190,9 @@ public class ReflectionRefractionTests {
     }
 
     @Test
+    /**
+     *
+     */
     public void Mp1() {
         Camera camera = new Camera(new Point3D(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
                 .setViewPlaneSize(200, 200).setDistance(1000);
